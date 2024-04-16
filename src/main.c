a99ed0be4110b6379b8134328f45756090aba0cd
Marco Palumbi <Marco.Palumbi@tii.ae>
2024-04-16 08:23:46 +0000

    apply permutation

--- src/main.c
+++ src/main.c
@@ -3,8 +3,12 @@
 #include "hal.h"
 
 #include <stdint.h>
+#include <string.h>
 
-typedef uint8_t bit_array_t[64];
+#define BIT_ARRAY_BYTE_SIZE 64
+#define PERM_SIZE  (BIT_ARRAY_BYTE_SIZE * 8)
+
+typedef uint8_t bit_array_t[BIT_ARRAY_BYTE_SIZE];
 #define ALIAS(bit_m) (uint32_t *)(0x22000000 + ((unsigned)((void *)bit_m - 0x20000000) * 32U))
 
 void main(void)
@@ -15,8 +19,16 @@ void main(void)
 
     bit_array_t volatile bit_array = {0xff,0x00};
     uint32_t volatile * const array_alias = ALIAS(bit_array);
+    uint16_t permutation[PERM_SIZE];
 
     printf("Alias address %X\n", array_alias);
+
+    // initialize permutation
+    for(int j = 0; j < PERM_SIZE; j++) {
+        permutation[j] = j;
+    }
+    permutation[1] = 10;
+    permutation[10] = 1;
 
     unsigned long long cycles_tot=0, cycles1, cycles2;
     double time_taken=0, start, end;
@@ -26,10 +38,12 @@ void main(void)
         printf("%0x %0X\n", bit_array[0], bit_array[1]);
         start = (double) clock();
         cycles1 = PLATFORM_CLOCK();
-        // test code here
-        uint32_t temp = array_alias[1];
-        array_alias[1] = array_alias[10];
-        array_alias[10] = temp;
+        // apply permutation
+        uint32_t buffer[PERM_SIZE];
+        for(int j = 0; j < PERM_SIZE; j++) {
+            buffer[permutation[j]] = array_alias[j];
+        }
+        memcpy((uint32_t *)array_alias, buffer, sizeof(buffer));
 
         cycles2 = PLATFORM_CLOCK();
         end = (double)clock();
